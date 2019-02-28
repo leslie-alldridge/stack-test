45a0fcca13281a8c19f57b577c5bef0a7a79ec1e
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-28 13:16:05 +1300

    test load state

--- tests/src/components/loading.test.js
+++ tests/src/components/loading.test.js
@@ -1,5 +1,15 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import "../../../setup.js";
+
 import Loading from '../../../src/components/Loading'
 
 it('renders without crashing', () => {
     expect(JSON.stringify(Loading)).toMatchSnapshot();
+  });
+
+  it('loads', () => {
+    const wrapper = shallow(<Loading />);
+    wrapper.setState({ loading: true })
+    expect(wrapper.state('loading')).toEqual(true)
   });
