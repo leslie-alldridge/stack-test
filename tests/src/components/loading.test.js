9a809fa25495c17c03f69c8a9ea679d14b051f93
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-02 07:15:35 +1300

    loading typo

--- tests/src/components/loading.test.js
+++ tests/src/components/loading.test.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import {shallow} from 'enzyme'
+import React from "react";
+import { shallow } from "enzyme";
 import "../../../setup.js";
 
-import Loading from '../../../src/components/Loading'
+import Loading from "../../../src/components/loading";
 
-it('renders without crashing', () => {
-    expect(JSON.stringify(Loading)).toMatchSnapshot();
-  });
+it("renders without crashing", () => {
+  expect(JSON.stringify(Loading)).toMatchSnapshot();
+});
 
-  it('loads', () => {
-    const wrapper = shallow(<Loading />);
-    wrapper.setState({ loading: true })
-    expect(wrapper.state('loading')).toEqual(true)
-  });
+it("loads", () => {
+  const wrapper = shallow(<Loading />);
+  wrapper.setState({ loading: true });
+  expect(wrapper.state("loading")).toEqual(true);
+});
