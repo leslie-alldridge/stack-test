3206f95907658b77d161ff10fc76deb00844dcb7
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-02 06:53:22 +1300

    max coverage

--- tests/src/components/saveOne.test.js
+++ tests/src/components/saveOne.test.js
@@ -21,6 +21,13 @@ describe("<SaveOne />", () => {
     wrap.setState({ name: "", age: "", location: "", error: false });
     expect(wrap.state("error")).toEqual(false);
   });
+
+  // it("sets state properly", () => {
+  //   const wrap = shallow(<SaveOne />);
+  //   wrap.setState({ name: "", age: "", location: "", error: "" });
+  //   expect(wrap.state("error")).toEqual(true);
+  // });
+
   describe("<DeleteOne />", () => {
     it("responds to name change", () => {
       const handleChangeSpy = sinon.spy(SaveOne.prototype, "handleChange");
@@ -34,7 +41,7 @@ describe("<SaveOne />", () => {
       const SaveOneSpy = sinon.spy(SaveOne.prototype, "save");
       const event = { target: { name: "num", value: 2 } };
       const wrap = mount(
-        <SaveOne state={{ cats: { err2: "err" } }} SaveOne={clickFn} />
+        <SaveOne state={{ cats: { error: "err" } }} SaveOne={clickFn} />
       );
       wrap.find("button").simulate("click", event);
 
@@ -45,7 +52,74 @@ describe("<SaveOne />", () => {
         location: "",
         error: true
       });
+
+      wrap.setState({ name: "", age: 2, location: "" });
+      wrap.find("button").simulate("click", event);
+
+      expect(wrap.state()).toEqual({
+        name: "",
+        age: 2,
+        location: "",
+        error: true
+      });
+
+      wrap.setState({ name: "test", age: "", location: "" });
+      wrap.find("button").simulate("click", event);
+
+      expect(wrap.state()).toEqual({
+        name: "test",
+        age: "",
+        location: "",
+        error: true
+      });
+
+      wrap.setState({ name: "test", age: 2, location: "" });
+      wrap.find("button").simulate("click", event);
+
+      expect(wrap.state()).toEqual({
+        name: "test",
+        age: 2,
+        location: "",
+        error: true
+      });
+
+      wrap.setState({ name: "", age: "", location: "test" });
+      wrap.find("button").simulate("click", event);
+
+      expect(wrap.state()).toEqual({
+        name: "",
+        age: "",
+        location: "test",
+        error: true
+      });
+      wrap.setState({ name: "test", age: 2, location: "test" });
+      wrap.find("button").simulate("click", event);
+
+      expect(wrap.state()).toEqual({
+        name: "",
+        age: "",
+        location: "",
+        error: false
+      });
     });
+
+    // it("button click should run function", () => {
+    //   const handleChangeSpy = sinon.spy(SaveOne.prototype, "save");
+    //   const event = { target: { name: "num", value: 2 } };
+    //   const wrap = mount(<SaveOne />);
+    //   wrap.find("button").simulate("click", event);
+    //   expect(handleChangeSpy.calledOnce).toEqual(true);
+    //   wrap.setState({ age: "" });
+
+    //   //expect(SaveOneSpy.calledOnce).toEqual(true);
+    //   expect(wrap.state()).toEqual({
+    //     name: test,
+    //     age: 2,
+    //     location: "",
+    //     error: true
+    //   });
+    // });
+
     describe("mapDispatchToProps", () => {
       it("should dispatch actions.onClick() when onClick() is called", () => {
         const dispatch = jest.fn();
